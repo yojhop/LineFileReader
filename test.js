73a15b6bb46e89194cec20ab09fd837106ac7c5a
yojhop <yojhop@126.com>
2020-08-22 13:39:26 +0800

    add vscode debugger

--- test.js
+++ test.js
@@ -3,6 +3,7 @@ const { read } = require('fs')
 let reader = new LineFileReader('./test.txt')
 
 function testInsert(series){
+  debugger
   if(series.length>0){
     let firstLine = series[0]
     let t = JSON.parse(firstLine)
@@ -16,6 +17,7 @@ function testInsert(series){
       if(obj2.ts<=ts) return -1
       if(obj1.ts>=ts) return 1
     }).then(res=>{
+      debugger
       let secondStart = res.secondStart
       series = series.filter(item=>{
         let obj = JSON.parse(item)
@@ -28,11 +30,12 @@ function testInsert(series){
         console.log('caught wrote error',e)
       })
     }).catch(e=>{
+      debugger
       console.log('caught error',e)
     })
   }
 }
-// testInsert(['{"ts":12,"k":12}','{"ts":13,"k":13}','{"ts":14,"k":14}','{"ts":15,"k":15}'])
+testInsert(['{"ts":12,"k":12}','{"ts":13,"k":13}','{"ts":14,"k":14}','{"ts":15,"k":15}'])
 // reader.getLastLine().then(res=>{
 //   console.log(res.data.toString())
 // })
